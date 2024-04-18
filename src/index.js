6d2839b8cdfd84d513c9098d69b68ac9463b2e84
Luis Pires <luisfpiress199@gmail.com>
2024-04-18 20:34:55 -0300

    update http to https

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
-const express = require('express');
+const https = require('https');
 const axios = require('axios');
 const fs = require('fs');
-const https = require('https');
-
+const express = require('express');
 const app = express();
-const PORT = 3333;
+
+
+const port = 3333;
 
 app.use(express.json());
 
-// Rota para intermediar a chamada de API
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post('/api/intermediary', async (req, res) => {
-  try {
-    const { url, method, body, headers } = req.body;
+    try {
+      const { url, method, body, headers } = req.body;
+  
+      // Carregando os certificados
+      const certificado = fs.readFileSync('/home/ubuntu/server10/cert/Certificado_itau.crt');
+      const chave = fs.readFileSync('/home/ubuntu/server10/cert/NOVO_CERTIFICADO.key');
+  
+      // Opções da requisição, incluindo certificados, corpo e cabeçalhos
+      const options = {
+        method: method,
+        url,
+        data: body, // Corpo da requisição
+        headers, // Cabeçalhos da requisição
+        
+      httpsAgent: new https.Agent({ // Passando os certificados
+          cert: certificado, // Certificado 1
+          key: chave, // Certificado 2
+        })
+      };
+  
+      // Fazendo a chamada ao servidor de destino
+      const response = await axios(options);
+  
+      // Enviando a resposta do servidor de destino de volta para o cliente
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
 
-    // Carregando os certificados
-    const certificado = fs.readFileSync('/home/ubuntu/server10/cert/Certificado_itau.crt');
-    const chave = fs.readFileSync('/home/ubuntu/server10/cert/NOVO_CERTIFICADO.key');
-
-    // Opções da requisição, incluindo certificados, corpo e cabeçalhos
-    const options = {
-      method: method,
-      url,
-      data: body, // Corpo da requisição
-      headers, // Cabeçalhos da requisição
-      
-    httpsAgent: new https.Agent({ // Passando os certificados
-        cert: certificado, // Certificado 1
-        key: chave, // Certificado 2
-      })
-    };
-
-    // Fazendo a chamada ao servidor de destino
-    const response = await axios(options);
-
-    // Enviando a resposta do servidor de destino de volta para o cliente
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Iniciar o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor intermediário está rodando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is listening on https://localhost:${port}`);
 });
