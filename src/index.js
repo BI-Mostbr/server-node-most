8f8cfdbdfc9cbed77db3990fb9bd8c2a20fc773e
Luis Pires <luisfpiress199@gmail.com>
2024-04-30 15:18:11 -0300

    adição da rota get, para dar get nas propostas

--- src/index.js
+++ src/index.js
@@ -54,6 +54,38 @@ app.post('/api/intermediary', async (req, res) => {
   }
 });
 
+// Get
+app.get('/api/intermediary', async (req, res) => {
+  try {
+    const { url, method, headers } = req.body;
+
+    // Carregando os certificados
+    const certificado = fs.readFileSync('/home/ubuntu/server-node-most-http/cert/Certificado_itau.crt');
+    const chave = fs.readFileSync('/home/ubuntu/server-node-most-http/cert/NOVO_CERTIFICADO.key');
+
+    // Opções da requisição, incluindo certificados, corpo e cabeçalhos
+    const options = {
+      method: method,
+      url,
+      headers, // Cabeçalhos da requisição
+      
+    httpsAgent: new https.Agent({ // Passando os certificados
+        cert: certificado, // Certificado 1
+        key: chave, // Certificado 2
+      })
+    };
+
+    // Fazendo a chamada ao servidor de destino
+    const response = await axios(options);
+
+    // Enviando a resposta do servidor de destino de volta para o cliente
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 // Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor intermediário está rodando na porta ${PORT}`);
